6a3ae14831f7e3fef55e064d3ce36839cf4c9c49
ApprehensiveKnee <edoardo.cabiati@mail.polimi.it>
2022-12-28 17:06:07 +0000

    Implemented index mapping and scaling methods

--- src/elements.cpp
+++ src/elements.cpp
@@ -5,7 +5,7 @@
 
 unsigned int Node::getId() const{return _id;};
 
-std::array<double, DIM> Node::getCoord() const{return _coord;};
+std::vector<double> Node::getCoord() const{return _coord;};
 
 void Node::setX(const double &r){_coord[0]=r; return;};
 
@@ -21,11 +21,76 @@ void Node::printNode(std::ostream &out) const
 };
 
 
+std::vector<std::vector<double>> Node::nodes1D(const unsigned int &nx, const Node &v)
+{
+    std::vector<std::vector<double>> temp(Segment::internalN(nx)); // vector containing the coordinates of the nodes along a certain dimension of the element
+    std::vector<double> v1= this->getCoord()/*get the coordinates of vert1*/;
+    std::vector<double> v2= v.getCoord()/*get the coordinates of vert2*/;
+    double hx = std::abs(v1[0] - v2[0])/nx;// step over x
+    for(unsigned int i = 0; i< nx; i ++) /*in this case, the last element v2x is manually inserted as to avoid round-off error discrepancies*/
+    {
+       temp[i].emplace_back( v1[0] + hx*i);
+    }
+    temp[nx].emplace_back(v2[0]);
+    return temp;
+};
+
+std::vector<std::vector<double>> Node::nodes2D(const unsigned int &n, const Node &v)
+{
+    std::vector<std::vector<double>> temp(Element_2D::internalN(n)); // vector containing the coordinates of the nodes along a certain dimension of the element
+    std::vector<double> v1= this->getCoord()/*get the cordinates of vert1*/;
+    std::vector<double> v2= v.getCoord()/*get the coordinates of vert2*/;
+    double hx = std::abs(v1[0] - v2[0])/n;// step over x
+    double hy = std::abs(v1[1] - v2[1])/n;// step over y
+
+    for(unsigned int i = 0; i<n ; ++i) /*in this case, the last element v2x is manually inserted as to avoid round-off error discrepancies*/
+    {
+       temp[i].emplace_back(v1[0] + hx*i);
+       temp[i].emplace_back(v1[1] + hy*i);
+    }
+
+    temp[n].emplace_back(v2[0]);
+    temp[n].emplace_back(v2[1]);
+    return temp;
+};
+
+
+
 //______________________________________________________________
 
 
 
-// ______________ DEFINITIONS FOR ELEMENT MEMBER FUNC. ______________
+// ______________ DEFINITIONS FOR 1D ELEMENT MEMBER FUNC. ______________
+
+
+unsigned int Segment::getId() const{return _element_id;};
+
+std::vector<unsigned int> Segment::getVert() const{return _vert;};
+
+void Segment::printElem(std::ostream &out) const
+{
+    //print the global index of the element
+    out<<"Vertices of Element #"<<_element_id<<" are:" << std::endl;
+    out << "----------"<<std::endl;
+    for (auto i : _vert)
+    {
+        //print the ids of the nodes of the element
+        out <<" "<< i <<" ";
+        
+    }
+    
+    out <<std::endl << "----------"<<std::endl;
+};
+
+
+unsigned int Segment::internalN(const unsigned int &nx){return nx + 1;};
+
+
+
+//______________________________________________________________
+
+
+// ______________ DEFINITIONS FOR 2D ELEMENT MEMBER FUNC. ______________
 
 
 unsigned int Element_2D::getId() const{return _element_id;};
@@ -49,12 +114,149 @@ void Element_2D::printElem(std::ostream &out) const
     out <<std::endl << "----------"<<std::endl;
 };
 
+unsigned int Element_2D::internalN(const unsigned int &nx){return (nx +1) ;};
 
 //______________________________________________________________
 
 
 
-// ______________ DEFINITIONS FOR MESH MEMBER FUNC. ______________
+
+// ______________ DEFINITIONS FOR 1D MESH MEMBER FUNC. ______________
+
+
+unsigned int Mesh_1D::get_nNodes() const{return _nNodes;};
+
+unsigned int Mesh_1D::get_nElems() const{return _nElems;};
+
+void Mesh_1D::printMesh(std::ostream& out) const
+{
+    out <<"_____________________________________"<< std::endl;
+    out << "The number of nodes of the mesh is: " << _nNodes << std::endl; 
+    out << "The number of elements of the mesh is: " << _nElems << std::endl; 
+    out << "The coordinates of the nodes are:"<<std::endl;
+    out << "Node: CoorX"<< std::endl;
+    for(auto i : _nodes){
+        i.printNode(out);
+    }
+    out << "The elements are:"<<std::endl;
+    for(auto i : _segments){
+        i.printElem(out);
+    }
+    
+
+};
+
+void Mesh_1D::setNum()
+{
+    //open a text file and read the parameters form it
+    std::ifstream global_numbers("../mesh/numbers.txt");
+    global_numbers >> _nNodes >> _nElems;
+    global_numbers.close();
+
+    _nodes.reserve(this->get_nNodes());
+    _segments.reserve(this->get_nElems());
+        
+    
+};
+  
+void Mesh_1D::setMesh()
+{
+    //generate and fill the mesh
+
+    //first set the number of nodes and elements by calling the setNum() method
+    setNum();
+    
+    //define some temp variables
+    std::string linestr, word;
+    unsigned int holder_id;
+    double holderX;
+    bool boundary;
+
+    
+    //read nodes coordinates from file
+    std::fstream nodes ("../mesh/nodes_coordinates.csv", std::ios::in);
+    if(nodes.is_open())
+    {
+        std::getline(nodes, linestr); // header row - throw away
+        while(std::getline(nodes, linestr))
+        {
+            //move the input date form one line to a stream
+            std::stringstream str(linestr);
+            //get the index of the nodes
+            std::getline(str, word,',');
+            std::istringstream(word)>>holder_id;
+            //get the first coordinate
+            std::getline(str,word,',');
+            std::istringstream(word)>>holderX;
+            //get the boundary value: determine whether the node is on the boundary
+            std::getline(str,word,',');
+            std::istringstream(word)>>boundary;
+            //create a temp Node and store it in the vector
+            Node tempnode(holder_id, holderX);
+            _nodes.emplace_back(tempnode);
+        }
+        nodes.close();
+    }
+    else
+    std::cout<<"Could not open the nodes file (nodes_coordinates)\n";
+
+
+    unsigned int holder1, holder2;
+    //read the elements from file
+    std::fstream elems ("../mesh/elements_vertexes.csv", std::ios::in);
+    if(elems.is_open())
+    {
+        std::getline(elems, linestr); // header row - throw away
+        while(std::getline(elems, linestr))
+        {
+            //move the input date form one line to a stream
+            std::stringstream str(linestr);
+            //get the index of the nodes
+            std::getline(str, word,',');
+            std::istringstream(word)>>holder_id;
+            //get the first vertex
+            std::getline(str,word,',');
+            std::istringstream(word)>>holder1;
+            //get the second vertex
+            std::getline(str,word,',');
+            std::istringstream(word)>>holder2;
+            
+            Segment temp(holder_id, holder1, holder2);
+            _segments.emplace_back(temp);
+            
+        }
+        elems.close();
+        
+    }
+    else
+    std::cout<<"Could not open the elements file (element_vertices)\n";
+
+    
+};
+
+std::vector<std::vector<unsigned int>> Mesh_1D::indexMapping(const unsigned int &degree, const unsigned int &nElements)
+{
+    unsigned int k (0);
+    //define the mapping vector as multidimensional vector having number of rows equal to the number of elements
+    //and for each element we construct the vector mapping the global index
+    std::vector<std::vector<unsigned int>> temp (k);
+    for(unsigned int j = 0; j <nElements; ++j)
+    {
+        for(unsigned int i = 1; i <= degree + 1; ++i){
+            temp[i-1].emplace_back(k+i);
+        }
+        k += degree;
+    }
+
+    return temp;
+
+};
+
+//______________________________________________________________
+
+
+
+// ______________ DEFINITIONS FOR 2D MESH MEMBER FUNC. ______________
 
 unsigned int Mesh_2D::get_nNodes() const {return _nNodes;};
 
@@ -169,7 +371,7 @@ void Mesh_2D::setMesh()
             //get the second vertex
             std::getline(str,word,',');
             std::istringstream(word)>>holder2;
-            //get the last vertx
+            //get the third vertx
             std::getline(str,word,',');
             std::istringstream(word)>>holder3;
             if(!_triatrue){
@@ -195,6 +397,82 @@ void Mesh_2D::setMesh()
 
     
 }
+
+//member function to map the global indexes of the nodes in the messh to the local indexes of the nodes inside in the elements
+//  THE GLOBAL INDEX OF THEE i LOCAL NODE OF A SPECIFIC ie ELEMENT CAN BE COMPUTED BY ACCESSING TO THE (i-1,ie-1) ELEMENT OF THE 
+//  VECTOR (MULTIDIMENSIONAL) RETUNED BY THE FUNCTION
+std::vector<std::vector<unsigned int>> Mesh_2D::indexMapping(  const unsigned int &degreeX/*degree of the polynomial along x axis*/, 
+                                                const unsigned int &nElementsX/*number of elements along x axis*/,
+                                                const unsigned int &degreeY/*degree of the polynomial along y axis*/,
+                                                const unsigned int &nElementsY/*number of elements along y axis*/)
+{
+    unsigned int npdx = degreeX +1;
+    unsigned int npdy = degreeY +1;
+    unsigned int ldnov=npdx*npdy;
+    unsigned ne=nElementsX*nElementsY;
+    std::vector<std::vector<unsigned int>> nov(ldnov, std::vector<unsigned int>(ne, 0));
+
+    //element 1
+
+    for(unsigned int i = 1; i <= ldnov; ++i)
+    {
+        nov[i-1][0]= i;
+    }
+
+    //elements first column
+
+    unsigned int k = ldnov - npdx;
+    for(unsigned int ie = 2; ie <= nElementsY; ie++){
+        for(unsigned int i = 1; i <= ldnov; ++i)
+        {
+            nov[i-1][ie-1] = i+k;
+        }
+    }
+    unsigned int kmax=k+npdx;
+
+    // other columns
+
+    unsigned int nxm1=npdx-1;
+    for (unsigned int iex = 2; iex <= nElementsX; ++iex){
+
+        //other rows, bottom elements
+        unsigned int ie = (iex - 1)*nElementsY + 1;
+        for(unsigned int j = 1; j <= npdy; ++j ){
+            k = (j-1)*npdx;
+            nov[k][ie-1] = nov[j*npdx-1][ie-nElementsY-1];
+            for(unsigned int i = 1; i <= nxm1; ++i)
+            {
+                nov[k+i][ie-1]= kmax+i;
+            }
+            kmax=kmax+nxm1;
+        }
+
+        //other elements
+        for(unsigned int iey = 2; iey <= nElementsY; ++iey){
+            ie=(iex-1)*nElementsY+iey;
+
+            //first row
+            for(unsigned int i = 1; i <= npdx; ++i)
+            {
+                nov[i-1][ie-1]=nov[ldnov-npdx+i-1][ie-2]; 
+            }
+            for(unsigned int j = 2; j <= npdy; ++j){
+                k=(j-1)*npdx;
+                nov[k][ie-1]=nov[j*npdx-1][ie-nElementsY-1];
+                for(unsigned int i = 1; i <= nxm1; ++i)
+                {
+                    nov[k+i][ie-1]=kmax+i; 
+                }
+                kmax=kmax+nxm1;
+            }
+            
+        }
+
+    }
+
+    return nov;
+
+};
 
 
 //______________________________________________________________
--- src/elements.hpp
+++ src/elements.hpp
@@ -8,9 +8,10 @@
 #include <iterator>
 #include <fstream>
 #include <sstream>
+#include <cmath>
 
 // Consider 2D problems
-inline constexpr unsigned DIM=2;
+inline constexpr unsigned DIM=1;
 
 
 class Node
@@ -19,28 +20,85 @@ private:
     //global id of the node
     unsigned int _id;
     //coordinates of the node
-    std::array<double, DIM > _coord;
+    std::vector<double> _coord;
 public:
     Node(){};
-    //constructor given the coordinates, dimensions and id
+    //constructor given the coordinates and id (1D case)
+    Node(const unsigned int &id, const double &x)
+        :_id(id),
+        _coord({x})
+        {};
+    //constructor given the coordinates and id (2D case)
     Node(const unsigned int &id, const double &x, const double &y)
         :_id(id),
-        _coord ({x,y})
+        _coord({x,y})
         {};
     //standard id getter
     unsigned int getId() const;
     //strandard coordinates getter
-    std::array<double, DIM > getCoord() const;
+    std::vector<double> getCoord() const;
     //standar setters
     void setX(const double &r);
     void setY(const double &s);
     //standard printer
     void printNode(std::ostream &out = std::cout) const;
+    //some methods to return a vector containing the coordinates of all the internal nodes 
+    //along a certain dimension of the element based off the degree of the polinomial used for such dimension
+
+    //the method for Segment(1D)
+    // in this case, nx represents the degree of the polynomial used while v is the other
+    //extremis of the segment for which we have to determine the coordinates of the internal nodes
+    std::vector<std::vector<double>> nodes1D(const unsigned int &nx, const Node &v);
+    //the methods for Element_2D(2D)
+    //in this case the method can be used either to compute the coordinates of the internal nodes over x
+    //or y directions: n is the degree of the polynomial chosen over a certain dimension, while v represent
+    // the other extremis of the side for which we want to determine the internal nodes
+    std::vector<std::vector<double>> nodes2D(const unsigned int &n/*either nx or ny*/, const Node &v/*either v1 or v3-v4 (triangles or rectangles)*/);
+
+
     //standard destructor
     ~Node() = default;
 };
 
 
+//Mesh element, 1D case
+
+class Segment
+{
+private:
+    //id of the element
+    unsigned int _element_id;
+    // global indexes of the nodes that compose the element
+    std::vector<unsigned int> _vert;
+public:
+    //default constructor
+    Segment() = default;
+    //constructor given the index of vertexes
+    Segment(const unsigned int &id, const unsigned int &vert1, const unsigned int &vert2)
+        :_element_id(id),
+        _vert({vert1, vert2})
+        {}
+    //constructor givenn a vector of vertexes
+    Segment(const unsigned int &id, const std::vector<unsigned int> vertexes)
+        :_element_id(id),
+        _vert(vertexes)
+        {}
+    //getter for the element id
+    unsigned int getId() const;
+    //getter for the ids of the vertexes
+    std::vector<unsigned int> getVert() const;
+    // printer: prints the id and vertexes of the element
+    void printElem(std::ostream &out = std::cout) const;
+    // a method to compute the number of internal nodes for each element, given the degree of the polunamilas used
+    // in each dimension for the spectral elements
+    static unsigned int internalN(const unsigned int &nx);
+    
+    //default destructor
+    ~Segment() = default;
+
+};
+
+// Base class, 2D element
 
 class Element_2D
 {
@@ -74,6 +132,11 @@ public:
     std::vector<unsigned int> getVert() const;
     //printer: prints the ids and coordinates of the vertexes
     void printElem(std::ostream &out = std::cout) const;
+    // a method to compute the number of internal nodes for each element, given the degree of the polunamilas used
+    // in each dimension for the spectral elements
+    static unsigned int internalN(const unsigned int &nx);
+    
+    
     //virtual default destructor
     virtual ~Element_2D(){};
 };
@@ -91,7 +154,6 @@ public:
     Triangle(const unsigned int &elem, std::vector<unsigned int> &vertexes)
         :Element_2D(elem, vertexes)
         {}
-    
     // virtual destructor overridden
     ~Triangle() = default;
 };
@@ -100,6 +162,19 @@ public:
 
 class Rectangle : public Element_2D
 {
+
+// the following is a viual representation of the ordering of vertices adopted for the construction
+// of the rectanglular element to be used for the SEM mesh
+//
+//                 side 3
+//            V4 __________ V3
+//              |          |
+//              |          |
+//      side 4  | Omega    |  side 2
+//              |          |
+//              |__________|
+//            V1            V2
+//                 side 1
 public:
     //constructor given the global indexes of the vertexes
     Rectangle(const unsigned int &elem,const unsigned int &vert1, const unsigned int &vert2, const unsigned int &vert3, const unsigned int &vert4)
@@ -109,15 +184,58 @@ public:
     Rectangle(const unsigned int &elem, std::vector<unsigned int> &vertexes)
         :Element_2D(elem, vertexes)
         {}
-    
+    //default destructor overridden
     ~Rectangle() = default;
 };
 
 
 
+// Mesh 1D case
 
 
-//----------------------------
+class Mesh_1D
+{
+private:
+    //number of total nodes of the mesh
+    unsigned int _nNodes;
+    //number of total elements of the mesh
+    unsigned int _nElems;
+    //vector containing the nodes and elements of the mesh
+    std::vector<Node> _nodes;
+    std::vector<Segment> _segments;
+public:
+    //standard constructor
+    Mesh_1D(){};
+    //constructor given the number of vertexes and elements
+    Mesh_1D(const unsigned int &V, const unsigned int &E)
+        :_nNodes(V),
+        _nElems(E)
+        {}
+    //getter for the number of Nodes
+    unsigned int get_nNodes() const;
+    //getter fotr the number of Elements
+    unsigned int get_nElems() const;
+    //standard printer
+    void printMesh(std::ostream& out = std::cout) const;
+    //setter for the number of elements and nodes for a txt file
+    void setNum();
+    //setter for the nodes and elements of the mesh
+    //this method reads the number of elements and nodes from a txt file,
+    //the nodes coordinates composing the mesh form a csv and finally the elements (to determine how to nodes are connected)
+    //for another csv file    
+    void setMesh();
+    //member function to map the global indexes of the nodes in the messh to the local indexes of the nodes inside in the elements
+    //  THE GLOBAL INDEX OF THEE i LOCAL NODE OF A SPECIFIC ie ELEMENT CAN BE COMPUTED BY ACCESSING TO THE (i-1,ie-1) ELEMENT OF THE 
+    //  VECTOR (MULTIDIMENSIONAL) RETUNED BY THE FUNCTION
+    static std::vector<std::vector<unsigned int>> indexMapping(const unsigned int &degree/*degree of the polynomial*/, const unsigned int &nElements/*number of elements*/);
+    //default destructor
+    ~Mesh_1D() = default;
+
+
+
+};
+
+//Mesh 2D case
 
 class Mesh_2D
 {
@@ -134,9 +252,9 @@ private:
     std::vector<Rectangle> _rectangles;
 public:
     //standard constructor
-    Mesh_2D(bool typ = true):_triatrue(typ){};
-    //constructor give the number of verteces and Elements (passed in code)
-    Mesh_2D(const unsigned int &V, const unsigned int &E, bool typ = true )
+    Mesh_2D(const bool &typ = true):_triatrue(typ){};
+    //constructor given the number of vertexes and Elements (passed as arguments in main)
+    Mesh_2D(const unsigned int &V, const unsigned int &E, const bool &typ = true )
         :_nNodes(V),
         _nElems(E),
         _triatrue(typ)
@@ -154,6 +272,32 @@ public:
     //the nodes coordinates composing the mesh form a csv and finally the elements (to determine how to nodes are connected)
     //for another csv file    
     void setMesh();
+    //member function to map the global indexes of the nodes in the messh to the local indexes of the nodes inside in the elements
+    //  THE GLOBAL INDEX OF THEE i LOCAL NODE OF A SPECIFIC ie ELEMENT CAN BE COMPUTED BY ACCESSING TO THE (i-1,ie-1) ELEMENT OF THE 
+    //  VECTOR (MULTIDIMENSIONAL) RETUNED BY THE FUNCTION
+
+
+    //             __________________________
+    //            |      |      |     |     |
+    //            |  3   |  6   |  9  | 12  |      Omega and spectral elements 
+    //            |      |      |     |     |      ordering
+    //            __________________________
+    //            |      |      |     |     |
+    //            |  2   |  5   |  8  | 11  |    
+    //            |      |      |     |     |
+    //            __________________________
+    //            |      |      |     |     |
+    //            |  1   |  4   |  7  | 10  |
+    //            |      |      |     |     |
+    //            __________________________
+
+
+
+    static std::vector<std::vector<unsigned int>>indexMapping(const unsigned int &degreeX/*degree of the polynomial along x axis*/, 
+                                                const unsigned int &nElementsX/*number of elements along x axis*/,
+                                                const unsigned int &degreeY/*degree of the polynomial along y axis*/,
+                                                const unsigned int &nElementsY/*number of elements along y axis*/);
+    ~Mesh_2D() = default;
 };
 
 
