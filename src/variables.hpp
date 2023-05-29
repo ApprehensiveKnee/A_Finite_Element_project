070cca6540580f7ab7fc27af850ddefef4b071f0
ApprehensiveKnee <edoardo.cabiati@mail.polimi.it>
2023-05-29 16:00:45 +0200

    added variables.hpp

---
+++ src/variables.hpp
@@ -0,0 +1,13 @@
+//===========================================================
+//             HEADER FILE FOR SOME GLOBAL VARIABLES
+//===========================================================
+
+
+#ifndef VAR
+#define VAR
+
+extern inline constexpr double tol= 1.e-16;
+extern inline constexpr unsigned r = 3;
+extern inline constexpr unsigned short _DIM = 1;
+
+#endif
